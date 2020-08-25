951fc6b429bf21d8b0dd6e2031504a39414e3198
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-25 21:23:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -31,8 +31,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, holiday|
     if season == "winter"
       holiday_hash.each do |holiday, supply|
-        if supply != "Balloons"
-          supply.add_
+        supply.add_if {|supply| }
       
     end
   end
