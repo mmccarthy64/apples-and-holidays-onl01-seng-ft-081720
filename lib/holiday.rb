141a52f696aadbeb9604ba72c632454d96b4f3f4
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-25 23:12:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,6 @@ def all_supplies_in_holidays(holiday_hash)
   
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-    binding.pry
     holiday_hash.each do |holiday, supplies|
       puts " #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
     end
